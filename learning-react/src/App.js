e93be533ffe0a8c17f1ff7a8b2ed26c06c0ffe46
akuthotayamuna <akuthota.yamuna95@gmail.com>
2023-07-26 10:24:03 -0500

    tic-tac-toe

---
+++ learning-react/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import Helloworld from './Component/Helloworld';
+import CounterExample from './Component/CounterExample';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+import Navigation from './Component/Navigation';
+
+function App() {
+  return (
+   <div>
+     {/* <Helloworld name="Arun"/> */}
+
+     {/* <CounterExample/> */}
+
+     <Header/>
+     <Footer/>
+     <Navigation/>
+   </div>
+    
+  );
+}
+
+export default App;
---
+++ learning-react/src/Component/Navigation.js
@@ -0,0 +1,16 @@
+ import React from 'react'
+ import { fontAwesomeIcon } from '@fortawesome/react-fontawesome'
+ //import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+ function Navigation() {
+    
+    return(
+        <nav>
+            <fontAwesomeIcon>
+            The nav
+            </fontAwesomeIcon>
+        </nav>
+    )
+ }
+
+ export default Navigation
